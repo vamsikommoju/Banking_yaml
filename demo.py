9bd25d7b5b68084c76759d563efb208bc3a39dec
vamsikommoju <satyasaivamsi79@gmail.com>
2023-10-24 13:48:27 +0530

    first commit

--- demo.py
+++ demo.py
@@ -35,17 +35,45 @@ class Information_yaml(Dictionary):
         version = data2['version']
         print(version)
 
-# class Servers_yaml(Information_yaml):
-#     def servers(self):
-#         self.list = []
-#         for i in self.dictionary:
-#             self.list.append(i)
-#         print(self.list)
+class Servers_yaml(Information_yaml):
+    def info(self):
+        self.list = []
+        for i in self.dictionary:
+            self.list.append(i)
+        return(self.list)
 
-#     def Server(self):
-#         data3 = self.dictionary[self.list[1]]
-#         servers = data3['servers']
-#         print(servers)
+    def data(self):
+        self.data = self.dictionary[self.list[2]]
+        return self.data
+    
+    def server_url(self):
+        self.data = self.dictionary[self.list[2]][0]
+        self.url = self.data['url']
+        return self.url
+    
+    def server_description(self):
+        self.data = self.dictionary[self.list[2]][0]
+        self.description = self.data['description']
+        return self.description
+
+class Paths(Dictionary):
+    def info(self):
+        self.list = []
+        for i in self.dictionary:
+            self.list.append(i)
+        self.data = self.dictionary[self.list[3]]
+        return(self.data) 
+
+class Accounts_get(Dictionary):
+    def info(self):
+        self.list = []
+        for i in self.dictionary:
+            self.list.append(i)
+        self.data = self.dictionary[self.list[3]]
+        self.accounts = self.data['/accounts']['get']
+        return self.accounts
+
+
 
 
 
@@ -57,6 +85,11 @@ Data2.info()
 Data2.Api()
 Data2.Description()
 Data2.Version()
-# Data3 = Servers_yaml("banking.yml")
-# Data3.yaml_reader()
-# Data3.Server()
+Data3 = Servers_yaml("banking.yml")
+Data3.yaml_reader()
+Data3.info()
+print(Data3.server_url())
+print(Data3.server_description())
+Data4 = Accounts_get("banking.yml")
+Data4.yaml_reader()
+Data4.info()
